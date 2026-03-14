6007fce19c17787053286f0a09da0e15688d58ef
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-03-14 18:50:53 -0300

    Added first map layer

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/**
+ * Main Program
+ */
+function main() {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
+
+// Start the main program when the DOM is ready
+document.addEventListener("DOMContentLoaded", () => main());
