bd4c159ac71b4095278f8a2322f47573baa9e8bc
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-03-15 01:05:16 -0300

    Added map layers and basic fetch

--- index.js
+++ index.js
@@ -1,44 +1,167 @@
 
 /**
+ * LatLong - Represents coordinates based on latitude and longitude.
+ */
+class LatLong {
+    latitude;
+    longitude;
+
+    /**
+     * 
+     * @param {number | number[]} latOrArray 
+     * @param {number | undefined} longitude 
+     * @returns 
+     */
+    constructor(latOrArray, longitude) {
+        if (arguments.length === 1) {
+            if (!Array.isArray(latOrArray) || typeof latOrArray[0] !== "number" || typeof latOrArray[1] !== "number")
+                throw Error("LatLong: latLong must be a 2 item array of numbers");
+            this.latitude = latOrArray[0];
+            this.longitude = latOrArray[1];
+        } else {
+            if (typeof latOrArray !== "number" || typeof longitude !== "number")
+                throw Error("LatLong: latitude and longitude must be a number");
+            this.latitude = latOrArray;
+            this.longitude = longitude;
+        }
+
+        return new Proxy(this, {
+            get(target, prop) {
+                if (prop === "0") return target.latitude;
+                if (prop === "1") return target.longitude;
+                return target[prop];
+            }
+        });
+    }
+
+    toString() {
+        return `LatLong(${this.latitude}, ${this.longitude})`;
+    }
+
+    toArray() {
+        return [this.latitude, this.longitude];
+    }
+
+    get lat() {
+        return this.latitude;
+    }
+
+    get long() {
+        return this.longitude;
+    }
+}
+
+
+class OpenSkyNetwork {
+    minBound;
+    maxBound;
+
+    constructor(minBound, maxBound) {
+        if (minBound && maxBound && minBound instanceof LatLong && maxBound instanceof LatLong) {
+            this.minBound = minBound;
+            this.maxBound = maxBound;
+        } else {
+            this.minBound = null;
+            this.maxBound = null;
+        }
+    }
+
+    #ROOT_URL = "https://opensky-network.org/api"
+
+    async #fetchJSON(url) {
+        const response = await fetch(url);
+        if (!response.ok)
+            throw new Error(`HTTP error! Status: ${response.status} ${response.statusText}`);
+        return await response.json();
+    }
+
+    #getBoundingParams() {
+        if (this.minBound && this.maxBound)
+            return `lamin=${this.minBound.lat}&lomin=${this.minBound.long}&lamax=${this.maxBound.lat}&lomax${this.maxBound.long}`;
+        else
+            return "";
+    }
+
+    async fetchAllStateVectors() {
+        const url = `${this.#ROOT_URL}/states/all?${this.#getBoundingParams()}`;
+        const json = await this.#fetchJSON(url);
+        return json;
+    }
+}
+
+
+/**
  * Main Program
  */
-function main() {
+async function main() {
 
     // Goal 1: Display a map in the browser.
 
-    // Get the map element from the page
-    const mapElement = document.getElementById('map');
+    // Define map center and data collection bounding box
+    const MAP_CENTER = new LatLong(44.87862, -63.50965);
+    const MIN_BOUND = new LatLong(MAP_CENTER.lat - 2, MAP_CENTER.long - 3);
+    const MAX_BOUND = new LatLong(MAP_CENTER.lat + 2, MAP_CENTER.long + 3);
 
-    // Initialize the map options
-    // See https://leafletjs.com/reference.html#map-option for more options
-    const mapOptions = {
-        // Center of YHZ runways
-        // Translated from https://www.openstreetmap.org/#map=19/44.887828/-63.505341
-        center: [44.887828, -63.505341],
+    // Initialize the map
+    // See https://leafletjs.com/reference.html#map-option for options
+    const map = L.map('map', {
+        center: MAP_CENTER.toArray(),
         zoom: 10,
-    };
+    });
 
-    // Create the map with the element and options constructor
-    const map = L.map(mapElement, mapOptions);
-
-    // Add the scale to the map
+    // Add the scale bar to the map
     L.control.scale().addTo(map);
 
     // Use OSM Tiles
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    // Flight data bounding box
+    const flightDataBoundBox = L.rectangle(
+        [
+            MIN_BOUND.toArray(),
+            MAX_BOUND.toArray()
+        ],
+        {
+            color: "#c0ffee",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0,
+            interactive: false,
+        }
+    ).addTo(map);
+
+    // MARKER: Halifax Airport (YHZ)
+    const hfxAirportMarker = L.marker(MAP_CENTER.toArray())
+        .bindPopup("<strong>Halifax Stanfield International Airport</strong><br/>ICAO: CYHZ<br/>IATA: YHZ")
+        .addTo(map);
+
+    // Add layer controls
+    const layerControl = L.control.layers(
+        // Base maps
+        {
+            "OpenStreetMap": osm
+        },
+        // Overlay maps
+        {
+            "Airport marker": hfxAirportMarker,
+            "Flight data bounding box": flightDataBoundBox
+        }).addTo(map);
 
     // Ideas to improve the map in the future
     // -   IDEA: Add pilot overlays to map
     // -   IDEA: Add drone overlays to map
 
     // TODO: Goal 2: Fetch real-time transit data information data from a publicly available API. (Flight data)
+    // API Docs: https://openskynetwork.github.io/opensky-api/rest.html
+    const osn = new OpenSkyNetwork(MIN_BOUND, MAX_BOUND);
+    console.log(await osn.fetchAllStateVectors());
 
     // TODO: Goal 3: Filter the raw data to a subset with specific criteria.
     // Sub-goals:
-    // 1.  Limit to canadian flights (arriving and/or departing)
+    // 1.  Limit to flights originating from Canada
 
     // TODO: Goal 4: Convert the filtered API data into GeoJSON format.
 
