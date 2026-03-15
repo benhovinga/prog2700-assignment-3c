f3ad0a02d3e9a3b8de49bba346279dbf7775c10b
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-03-14 22:17:28 -0300

    Cleaned up experiments and add goals

--- index.js
+++ index.js
@@ -3,55 +3,55 @@
  * Main Program
  */
 function main() {
+
+    // Goal 1: Display a map in the browser.
+
+    // Get the map element from the page
     const mapElement = document.getElementById('map');
+
+    // Initialize the map options
+    // See https://leafletjs.com/reference.html#map-option for more options
     const mapOptions = {
-        center: [51.505, -0.09],
-        zoom: 13,
-        attributionControl: false,
-        zoomControl: false,
-        closePopupOnClick: false,
-        boxZoom: false,
-        doubleClickZoom: false,
-        dragging: false,
-        scrollWheelZoom: false,
-        keyboard: false,
-        touchZoom: false,
+        // Center of YHZ runways
+        // Translated from https://www.openstreetmap.org/#map=19/44.887828/-63.505341
+        center: [44.887828, -63.505341],
+        zoom: 10,
     };
+
+    // Create the map with the element and options constructor
     const map = L.map(mapElement, mapOptions);
 
+    // Add the scale to the map
+    L.control.scale().addTo(map);
+
+    // Use OSM Tiles
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    var marker = L.marker([51.5, -0.09]).addTo(map);
+    // Ideas to improve the map in the future
+    // -   IDEA: Add pilot overlays to map
+    // -   IDEA: Add drone overlays to map
 
-    const circle = L.circle([51.508, -0.11], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-    }).addTo(map);
+    // TODO: Goal 2: Fetch real-time transit data information data from a publicly available API. (Flight data)
 
-    const polygon = L.polygon([
-        [51.509, -0.08],
-        [51.503, -0.06],
-        [51.51, -0.047]
-    ]).addTo(map);
+    // TODO: Goal 3: Filter the raw data to a subset with specific criteria.
+    // Sub-goals:
+    // 1.  Limit to canadian flights (arriving and/or departing)
 
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-    circle.bindPopup("I am a circle.");
-    polygon.bindPopup("I am a polygon.");
-
-    const popup = L.popup()
+    // TODO: Goal 4: Convert the filtered API data into GeoJSON format.
 
 
-    // map.on('click', (e) => {
-    //     popup
-    //         .setLatLng(e.latlng)
-    //         .setContent("You clicked the map at " + e.latlng.toString())
-    //         .openOn(map);
-    // });
+    // TODO: Goal 5: Plot markers on the map to display the current position of vehicles.
+    // Sub-goals:
+    // 1.  Only selected markers are plotted
+    // 2.  Use custom images of planes
+    // 3.  Plane is facing the correct direction (bearing)
+    // 4.  Clicks show pop-up with flight information
+
+    // TODO: Goal 6: Add functionality that will cause the map to auto refresh after a certain interval of time.
+
 }
 
 // Start the main program when the DOM is ready
